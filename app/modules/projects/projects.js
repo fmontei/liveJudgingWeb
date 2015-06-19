2cedbe712d5cec5bee5ce9090292718729d9cb94
fmontei <felipe.carneiro.monteiro@gmail.com>
2015-06-18 21:15:15 -0400

    Categories can now be edited.

--- app/modules/projects/projects.js
+++ app/modules/projects/projects.js
@@ -15,24 +15,24 @@ angular.module('liveJudgingAdmin.projects', ['ngRoute', 'ngCookies', 'liveJudgin
 
 		$cookies.categoryList = [{
 			'name': "Uncategorized",
-			'desc': "",
-			'time': "",
+			'desc': "Sample description for Uncategorized",
+			'time': "1:00am",
 			'color': "#f0ad4e",
 			'projects': ["Tiger"],
 			'judges': []
 		}, 
 		{
 			'name': "Cat A.",
-			'desc': "",
-			'time': "",
+			'desc': "Sample description for Cat A.",
+			'time': "2:00pm",
 			'color': "#5bc0de",
 			'projects': ["Lemur"], 
 		  'judges': ["Batman"]
 		},
 		{
 			'name': "Cat B.",
-			'desc': "",
-			'time': "",
+			'desc': "Sample description for Cat B.",
+			'time': "3:00pm",
 			'color': "#d9534f",
 			'projects': ["Lion", "Monkey"], 
 			'judges': ["Superman", "Hulk"]
@@ -43,8 +43,9 @@ angular.module('liveJudgingAdmin.projects', ['ngRoute', 'ngCookies', 'liveJudgin
 		$scope.currentView = "default";
 		$scope.categoryList = $cookies.categoryList;
 		$scope.projectList = $cookies.projectList;
+		$scope.categoryModalView = "";
 
-		$scope.timeOptions = function() {
+		$scope.getTimeOptions = function() {
 			var times = [];
 			for (var i = 1; i <= 12; i++) {
 				for (var j = 0; j <= 45; j += 15) {
@@ -54,6 +55,7 @@ angular.module('liveJudgingAdmin.projects', ['ngRoute', 'ngCookies', 'liveJudgin
 			}
 			return times;
 		}
+		$scope.timeOptions = $scope.getTimeOptions();
 
 		$scope.changeView = function(view) {
 			$scope.currentView = view;
@@ -74,6 +76,21 @@ angular.module('liveJudgingAdmin.projects', ['ngRoute', 'ngCookies', 'liveJudgin
 			console.log("New category created: " + JSON.stringify(newCategory));
 		}
 
+		$scope.editSelectedCategory = function() {
+			var updatedCategoryPeriod = ($scope.updatedCategoryPeriod === true) ? 'am' : 'pm';
+			var updatedCategory = {
+				name: $scope.updatedCategoryName,
+				desc: $scope.updatedCategoryDesc,
+				time: $scope.updatedCategoryTime + updatedCategoryPeriod,
+				color: $scope.updatedCategoryColor,
+				projects: $scope.categoryToEdit.projects,
+				judges: $scope.categoryToEdit.judges
+			}
+			$scope.closeCategoryModal();
+			$scope.updateCategoryCookie('name', $scope.categoryName, updatedCategory);
+			console.log("Category edited: " + JSON.stringify(updatedCategory));
+		}
+
 		$scope.seeCategoryDetails = function(event) {
 			var category = $(event.currentTarget);
 			var categoryName = category.find('h3').text().trim();
@@ -88,14 +105,63 @@ angular.module('liveJudgingAdmin.projects', ['ngRoute', 'ngCookies', 'liveJudgin
 			}
 		}
 
-		$scope.closeCategoryModal = function(e) {
+		$scope.updateCategoryCookie = function(param, value, newObject) {
+			if (param === 'name') {
+				for (var i = 0; i < $cookies.categoryList.length; i++) {
+					if ($cookies.categoryList[i].name == value) {
+						$cookies.categoryList[i] = newObject;
+					}
+				}
+			}
+		}
+
+		$scope.closeCategoryModal = function(event) {
 			$($scope.categoryModalID).modal('hide');
 		}
 
-		$scope.showCategoryModal = function(e) {
-			e.stopPropagation();
+		$scope.changeCategoryModalViewToCreate = function() {
+			$scope.categoryModalView = 'create';
 			$($scope.categoryModalID).modal('show');
 		}
+
+		$scope.changeCategoryModalViewToEdit = function(event, category) {
+			$scope.categoryModalView = 'edit';
+			$scope.categoryToEdit = category;
+
+			var time, period;
+			if (category.time.search('a') !== -1) {
+				period = true; // true == am
+				time = category.time.split('a')[0];
+			} else {
+				period = false; // false == pm
+				time = category.time.split('p')[0];
+			}
+			
+			$scope.categoryName = category.name;
+			$scope.categoryDesc = category.desc;
+			$scope.categoryTime = time;
+			$scope.categoryPeriod = period;
+			$scope.categoryColor = category.color; 
+
+			event.stopPropagation(); // Prevent button behind it from redirecting the page
+			$($scope.categoryModalID).modal('show'); // Show the modal
+		}
+
+		$scope.$watch('categoryName', function(value) {
+      $scope.updatedCategoryName = value
+    });
+    $scope.$watch('categoryDesc', function(value) {
+    	$scope.updatedCategoryDesc = value
+    });
+    $scope.$watch('categoryTime', function(value) {
+    	$scope.updatedCategoryTime = value
+    });
+    $scope.$watch('categoryPeriod', function(value) {
+    	$scope.updatedCategoryPeriod = value
+    });
+    $scope.$watch('categoryColor', function(value) {
+    	$scope.updatedCategoryColor = value
+    });
 
 }])
 
@@ -192,11 +258,19 @@ angular.module('liveJudgingAdmin.projects', ['ngRoute', 'ngCookies', 'liveJudgin
 	return {
 		restrict: 'A',
 		require: '^ngModel',
+		scope: {
+			state: '@text'
+		},
 		link: function(scope, elem, attrs) {
 			elem.bootstrapSwitch({
 				onText: 'am',
-				offText: 'pm',
-				state: 'true' // Default to 'am'
+				offText: 'pm', 
+				state: true
+			});
+			scope.$watch('state', function(value) {
+				var booleanVal = value === 'true';
+				console.log(value);
+			
 			});
 		}
 	};
