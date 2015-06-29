685968d5b1887045f6d02ae8f7a3c54e5d042085
Felipe Monteiro <fmontei@g.clemson.edu>
2015-06-29 03:53:40 -0400

    Added rubric html/rubric modal.

--- app/modules/rubrics/rubrics.js
+++ app/modules/rubrics/rubrics.js
@@ -9,6 +9,21 @@ angular.module('liveJudgingAdmin.rubrics', ['ngRoute'])
   });
 }])
 
-.controller('RubricsCtrl', [function() {
-
+.controller('RubricsCtrl', ['$scope', function($scope) {
+	$scope.modalCriteria = [{name: 'Flavor'}, {name: 'Texture'}, {name: 'Size'}];
+	
+	$scope.createNewAccordionCriterion = function() {
+		var numberOfNewCriteria = 0;
+		for (var i = 0; i < $scope.modalCriteria.length; i++) 
+			if ($scope.modalCriteria[i].name.indexOf('New Criterion') !== -1)
+				numberOfNewCriteria++;
+		if (numberOfNewCriteria > 0)
+			$scope.modalCriteria.push({name: 'New Criterion (' + numberOfNewCriteria + ')'});
+		else
+			$scope.modalCriteria.push({name: 'New Criterion'});
+	}
+	
+	$scope.removeAccordionCriterion = function(index) {
+		$scope.modalCriteria.splice(index, 1);
+	}
 }]);
