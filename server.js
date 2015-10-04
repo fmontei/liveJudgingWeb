63aab8c13f21100dd9644faec7300afebf8b6570
Felipe Monteiro <fmontei@g.clemson.edu>
2015-10-04 11:20:43 -0400

    Another package.json/server.js change.

--- server.js
+++ server.js
@@ -1,23 +1,14 @@
-'use strict';
- 
 var express = require('express');
-var http = require('http');
-var path = require('path');
-var favicon = require('serve-favicon');
-var morgan = require('morgan'); // formerly express.logger
-var errorhandler = require('errorhandler');
 var app = express();
- 
-// all environments
-app.set('port', process.env.PORT || 5000);
- 
-// express/connect middleware
-app.use(morgan('dev'));
- 
-// serve up static assets
-app.use(express.static(path.join(__dirname, 'app')));
- 
-http.createServer(app).listen(app.get('port'), function () {
-  console.log('myApp server listening on port ' + app.get('port'));
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/app'));
+
+app.get('/', function(request, response) {
+  response.render('app/app.js');
 });
 
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
