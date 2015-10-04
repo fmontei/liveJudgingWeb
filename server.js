ac7af4caa1c1a106492c2c233d01b8f87f68afdc
Felipe Monteiro <fmontei@g.clemson.edu>
2015-10-04 11:07:12 -0400

    Modified server.js.

--- server.js
+++ server.js
@@ -2,16 +2,10 @@
  
 var express = require('express');
 var http = require('http');
-var path = require('path');
-var favicon = require('serve-favicon');
-var morgan = require('morgan'); // formerly express.logger
-var errorhandler = require('errorhandler');
 var app = express();
  
 // all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.engine('html', require('ejs').renderFile);
+app.set('port', process.env.PORT || 5000);
  
 // express/connect middleware
 app.use(morgan('dev'));
@@ -19,11 +13,7 @@ app.use(morgan('dev'));
 // serve up static assets
 app.use(express.static(path.join(__dirname, 'app')));
  
-// development only
-if ('development' === app.get('env')) {
-  app.use(errorhandler());
-}
- 
 http.createServer(app).listen(app.get('port'), function () {
   console.log('myApp server listening on port ' + app.get('port'));
 });
+
